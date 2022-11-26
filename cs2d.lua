b56ec8434690c7104d7a8dc9bc647863f6ebbbf8
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-11-26 09:10:58 +0800

    hmm

---
+++ cs2d.lua
@@ -0,0 +1,198 @@
+---  @meta
+
+--[[
+THIS IS A META FILE FOR CS2D COMPLETIONS,
+DON'T `dofile` THIS FILE!
+]]
+
+--- @alias addhook_hook
+---| '"always"'
+---| '"assist"'
+---| '"attack"'
+---| '"attack2"'
+---| '"bombdefuse"'
+---| '"bombexplode"'
+---| '"bombplant"'
+---| '"break"'
+---| '"build"'
+---| '"buildattempt"'
+---| '"buy"'
+---| '"clientdata"'
+---| '"clientsetting"'
+---| '"collect"'
+---| '"connect"'
+---| '"connect_attempt"'
+---| '"connect_initplayer"'
+---| '"die"'
+---| '"disconnect"'
+---| '"dominate"'
+---| '"drop"'
+---| '"endround"'
+---| '"flagcapture"'
+---| '"flagtake"'
+---| '"flashlight"'
+---| '"hit"'
+---| '"hitzone"'
+---| '"hostagedamage"'
+---| '"hostagekill"'
+---| '"hostagerescue"'
+---| '"hostageuse"'
+---| '"httpdata"'
+---| '"itemfadeout"'
+---| '"join"'
+---| '"key"'
+---| '"kill"'
+---| '"leave"'
+---| '"log"'
+---| '"mapchange"'
+---| '"menu"'
+---| '"minute"'
+---| '"move"'
+---| '"movetile"'
+---| '"ms100"'
+---| '"name"'
+---| '"objectdamage"'
+---| '"objectkill"'
+---| '"objectupgrade"'
+---| '"parse"'
+---| '"projectile"'
+---| '"projectile_impact"'
+---| '"radio"'
+---| '"rcon"'
+---| '"reload"'
+---| '"say"'
+---| '"sayteam"'
+---| '"sayteamutf8"'
+---| '"sayutf8"'
+---| '"second"'
+---| '"select"'
+---| '"serveraction"'
+---| '"shieldhit"'
+---| '"shutdown"'
+---| '"spawn"'
+---| '"specswitch"'
+---| '"spray"'
+---| '"startround"'
+---| '"startround_prespawn"'
+---| '"suicide"'
+---| '"team"'
+---| '"trigger"'
+---| '"triggerentity"'
+---| '"turretscan"'
+---| '"use"'
+---| '"usebutton"'
+---| '"vipescape"'
+---| '"voice"'
+---| '"vote"'
+---| '"walkover"'
+
+--[[
+Attaches the Lua function "func" to the hook "hook". CS2D will always call that Lua function when the event related to that hook occurs.
+Priority is only relevant if there is more than one function attached to the same hook.
+<s>
+]]
+---@param hook addhook_hook
+---@param func string
+---@param prio? number
+function addhook(hook,func,prio) end
+
+
+--- @alias addbind_key
+---| '"0"'
+---| '"1"'
+---| '"2"'
+---| '"3"'
+---| '"4"'
+---| '"5"'
+---| '"6"'
+---| '"7"'
+---| '"8"'
+---| '"9"'
+---| '"A"'
+---| '"B"'
+---| '"C"'
+---| '"D"'
+---| '"E"'
+---| '"F"'
+---| '"G"'
+---| '"H"'
+---| '"I"'
+---| '"J"'
+---| '"K"'
+---| '"L"'
+---| '"M"'
+---| '"N"'
+---| '"O"'
+---| '"P"'
+---| '"Q"'
+---| '"R"'
+---| '"S"'
+---| '"T"'
+---| '"U"'
+---| '"V"'
+---| '"W"'
+---| '"X"'
+---| '"Y"'
+---| '"Z"'
+---| '"mouse1"'
+---| '"mouse2"'
+---| '"mouse3"'
+---| '"mwheelup"'
+---| '"mwheeldown"'
+---| '"leftshift"'
+---| '"rightshift"'
+---| '"leftctrl"'
+---| '"rightctrl"'
+---| '"leftalt"'
+---| '"rightalt"'
+---| '"leftsys"'
+---| '"rightsys"'
+---| '"leftarrow"'
+---| '"rightarrow"'
+---| '"uparrow"'
+---| '"downarrow"'
+---| '"backspace"'
+---| '"tab"'
+---| '"clear"'
+---| '"enter"'
+---| '"escape"'
+---| '"space"'
+---| '"pgup"'
+---| '"pgdown"'
+---| '"end"'
+---| '"home"'
+---| '"select"'
+---| '"print"'
+---| '"execute"'
+---| '"screen"'
+---| '"ins"'
+---| '"del"'
+
+--[[
+Adds a Lua key bind for the specified key. This bind will be active for all players and will trigger the key-hook when a player presses or releases the related key.
+Returns 1 if adding the key bind worked, 0 otherwise (invalid key name/already bound).
+<s>
+]]
+---@param key addbind_key
+---@return number
+function addbind(key) end
+
+
+---@alias player_value
+---| '"exists"'
+---| '"name"'
+---| '"ip"'
+---| '"port"'
+---| '"usgn"'
+---| '"usgnname"'
+---| '"steamid"'
+---| '"steamname"'
+---| '"bot"'
+---| '"rcon"'
+---| '"language"'
+---| '"language_iso"'
+
+---@param id number
+---@param value player_value
+---@return boolean|string|number
+function player(id,value) end
